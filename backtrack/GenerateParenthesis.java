ce354c99240601b55303ad7e18ba7cc7da628a39
Juli <julianalicode@gmail.com>
2024-01-13 23:30:43 -0500

    create GenerateParenthesis

---
+++ backtrack/GenerateParenthesis.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParenthesis {
+    public List<String> generateParenthesis(int n){
+       List<String> list = new ArrayList<>();
+       backtrack("", list, n, 0, 0);
+       return list;
+    }
+    public void backtrack(String temp, List<String> list, int n, int open, int close){
+        if(temp.length()==n*2){
+            list.add(temp);
+            return;
+        }
+        if(open<n){
+            backtrack(temp+"(", list, n, open+1, close);
+        }
+        if(close<n){
+            backtrack(temp+")", list, n, open, close+1);
+        }
+    }
+}
