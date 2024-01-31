60b6b04fbb93eed90a549d3582e194debdd197b2
Juli <julianalicode@gmail.com>
2024-01-30 23:26:30 -0500

    remove element

---
+++ sort/removeDuplicate.java
@@ -0,0 +1,13 @@
+public class removeDuplicate {
+    public int remove(int[] nums){
+        int p =0;
+        for(int i =1; i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[p]=nums[i];
+
+                p++;
+            }
+        }
+        return p;
+    }
+}
---
+++ sort/removeElement.java
@@ -0,0 +1,13 @@
+package sort;
+public class removeElement {
+    public int removeElement(int[] nums,int val){
+        int p =0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[p]=nums[i];
+                p++;
+            }
+        }
+        return p;
+    }
+}
