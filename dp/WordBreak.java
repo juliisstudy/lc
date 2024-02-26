84900a0505e44e9997b63165fcf0f13a7f46f895
Juli <julianalicode@gmail.com>
2024-02-26 17:01:01 -0500

    word break

---
+++ dp/WordBreak.java
@@ -0,0 +1,23 @@
+package dp;
+
+import java.util.List;
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict){
+        boolean[] dp = new boolean[s.length()];
+        for(int i=0;i<s.length();i++){
+            for(String word:wordDict){
+                if(i<word.length()-1){
+                    continue;
+                }
+                if(i==word.length()||dp[i-word.length()]){
+                    if(s.substring(i-word.length()+1, i+1).equals(word)){
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[s.length()-1];
+    }
+}
